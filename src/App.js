bdeae4c78646d32ac357c8d23183439b6fb98c5a
Siddharth <dsiddharth2@gmail.com>
2022-11-27 15:45:38 +0530

    Introduced the basic function for the website

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import "./App.scss";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      resumeData: {},
+      sharedData: {}
+    };
+  }
+
+  applyPickedLanguage() {
+    var resumePath = `en.json`;
+    this.loadResumeFromPath(resumePath);
+  }
+
+  componentDidMount() {
+    this.loadSharedData();
+    this.applyPickedLanguage();
+  }
+
+  loadResumeFromPath(path) {
+    $.ajax({
+      url: path,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  loadSharedData() {
+    $.ajax({
+      url: `portfolio.json`,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ sharedData: data });
+        document.title = `${this.state.sharedData.basic_info.name}`;
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <About
+          resumeBasicInfo={this.state.resumeData.basic_info}
+          sharedBasicInfo={this.state.sharedData.basic_info}
+        />
+        <Projects
+          resumeProjects={this.state.resumeData.projects}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Skills
+          sharedSkills={this.state.sharedData.skills}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Experience
+          resumeExperience={this.state.resumeData.experience}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+      </div>
+    );
+  }
+}
+
+export default App;
